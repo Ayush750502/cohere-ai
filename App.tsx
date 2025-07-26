3b33743bd6b3f5cb08824df600449410a4b1523d
Ayush750502 <ayush750502@gmail.com>
2025-07-26 20:56:15 +0530

    adding the recent changes toi the app file and testing the login function

--- App.tsx
+++ App.tsx
@@ -3,10 +3,12 @@ import { View, Text } from 'react-native';
 
 import { styles } from "./styles";
 
+import Login from './screens/login';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Sample Text </Text>
+      <Login />
     </View>
   );
 }
