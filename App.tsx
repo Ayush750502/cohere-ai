9dda457c71b82c3440980eb9d04f95190aa3e3ea
Ayush750502 <ayush750502@gmail.com>
2025-07-26 16:52:43 +0530

    Adding sample text

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { styles } from "./styles";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <Text> Sample Text </Text>
     </View>
   );
 }
