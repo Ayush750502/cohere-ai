2c2e419606fa396d91e5b2e58ec7dadf0cd7dfe2
Ayush750502 <ayush750502@gmail.com>
2025-07-26 20:55:47 +0530

    Login Screen where user will login themselves

---
+++ screens/Login.tsx
@@ -0,0 +1,9 @@
+
+import { Text, View } from 'react-native'
+import { styles } from '../styles';
+
+export default function Login(){
+    return<View style={styles.container}>
+        <Text>Login Page</Text>
+    </View>;
+}
