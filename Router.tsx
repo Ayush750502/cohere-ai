bbfebfc18b88ef41411a146b3ffa4d2095fe5af4
Ayush750502 <ayush750502@gmail.com>
2025-07-26 23:53:49 +0530

    adding RootStackParamasList type to the Stack

--- Router.tsx
+++ Router.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createNativeStackNavigator();
+import { RootStackParamList } from './defination';
 
 import Login from './screens/Login';
+import SignUp from './screens/SignUp';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Router(){
     return(<NavigationContainer>
         <Stack.Navigator initialRouteName="Login" >
             <Stack.Screen name="Login"  component={Login} />
+            <Stack.Screen name="SignUp"  component={SignUp} />
         </Stack.Navigator>
     </NavigationContainer>);
 }
