16c6d189f3f2643749d2f865af0e8f01d7cbf58c
Ayush750502 <ayush750502@gmail.com>
2025-08-01 19:51:50 +0530

    adding props definations for the screens of AppDrawer, Dashboard and Chat

--- defination.ts
+++ defination.ts
@@ -1,4 +1,39 @@
+// defination.ts
+
 export type RootStackParamList = {
-    Login: undefined,
-    SignUp: undefined
+    Auth: undefined;
+  Login: undefined;
+  SignUp: undefined;
+  AppDrawer: undefined;
+  Dashboard: undefined;
+  Chat: { conversationId?: number };
+};
+
+export type AppDrawerParamList = {
+  Chat: { conversationId?: number };
+};
+
+export type User = {
+  username: string; // full name of the user
+  email: string; // unique id for the user
+  password: string; // password for authentication
+  dob: Date; // Date of birth
+  phone: number; // user's mobile number
+  profileImage: string; // To store address for user's profile image
+  conversations: number[]; // list of conversation IDs
+};
+
+export type conversations = {
+  id: number;
+  conversation: chat[];
+};
+
+export enum role {
+  user = 0,
+  assistant = 1,
+}
+
+export type chat = {
+  role: role;
+  message: string;
 };
